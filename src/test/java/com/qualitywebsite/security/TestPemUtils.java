12197a226b7b6ddeb6ee2c094524c24d80054066
Srikanthy07 <srikanthy0707@gmail.com>
2026-08-17 11:25:44 +0530

    updated files

--- src/test/java/com/qualitywebsite/security/TestPemUtils.java
+++ src/test/java/com/qualitywebsite/security/TestPemUtils.java
@@ -1,5 +1,7 @@
 package com.qualitywebsite.security;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.PrivateKey;
@@ -9,27 +11,41 @@ import java.util.Base64;
 public class TestPemUtils {
 
     public static String[] generateValidPemPair() {
-        try (InputStream is = TestPemUtils.class.getResourceAsStream("/testkeystore.p12")) {
+        try {
+            InputStream is = TestPemUtils.class.getResourceAsStream("/testkeystore.p12");
             if (is == null) {
-                throw new IllegalStateException("Classpath resource /testkeystore.p12 not found!");
+                File file = new File("src/test/resources/testkeystore.p12");
+                if (file.exists()) {
+                    is = new FileInputStream(file);
+                } else {
+                    file = new File("d:/quality-backend/src/test/resources/testkeystore.p12");
+                    if (file.exists()) {
+                        is = new FileInputStream(file);
+                    }
+                }
             }
-            KeyStore ks = KeyStore.getInstance("PKCS12");
-            ks.load(is, "password".toCharArray());
-            String alias = ks.aliases().nextElement();
-            Certificate cert = ks.getCertificate(alias);
-            PrivateKey key = (PrivateKey) ks.getKey(alias, "password".toCharArray());
+            if (is == null) {
+                throw new IllegalStateException("Could not locate testkeystore.p12 via classpath or file path");
+            }
+            try (InputStream input = is) {
+                KeyStore ks = KeyStore.getInstance("PKCS12");
+                ks.load(input, "password".toCharArray());
+                String alias = ks.aliases().nextElement();
+                Certificate cert = ks.getCertificate(alias);
+                PrivateKey key = (PrivateKey) ks.getKey(alias, "password".toCharArray());
 
-            String certPem = "-----BEGIN CERTIFICATE-----\n" +
-                    Base64.getMimeEncoder(64, new byte[]{'\n'}).encodeToString(cert.getEncoded()) +
-                    "\n-----END CERTIFICATE-----\n";
+                String certPem = "-----BEGIN CERTIFICATE-----\n" +
+                        Base64.getMimeEncoder(64, new byte[]{'\n'}).encodeToString(cert.getEncoded()) +
+                        "\n-----END CERTIFICATE-----\n";
 
-            String keyPem = "-----BEGIN PRIVATE KEY-----\n" +
-                    Base64.getMimeEncoder(64, new byte[]{'\n'}).encodeToString(key.getEncoded()) +
-                    "\n-----END PRIVATE KEY-----\n";
+                String keyPem = "-----BEGIN PRIVATE KEY-----\n" +
+                        Base64.getMimeEncoder(64, new byte[]{'\n'}).encodeToString(key.getEncoded()) +
+                        "\n-----END PRIVATE KEY-----\n";
 
-            return new String[]{certPem, keyPem};
+                return new String[]{certPem, keyPem};
+            }
         } catch (Exception e) {
-            throw new RuntimeException("Failed to load test PEM pair from classpath: /testkeystore.p12", e);
+            throw new RuntimeException("Failed to load test PEM pair from testkeystore.p12", e);
         }
     }
 }
